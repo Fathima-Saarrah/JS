0851555b9b275b18147fef2700523579676ff3e8
Fathima-Saarrah <saarrahstar@gmail.com>
2026-03-05 16:18:36 +0530

    changed

--- dates.js
+++ dates.js
@@ -1,4 +1,4 @@
-const d = new Date(); //Date class is a deafult class
+const d = new Date(); //Date class is a default class
 console.log(d);
 const d1 = new Date("October 13, 2014 11:13:00");
 console.log(d1); //to print d1 
